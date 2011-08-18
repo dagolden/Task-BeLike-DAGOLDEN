0efab621150a145686b96e97fea52e5e7919ba57
David Golden <dagolden@cpan.org>
2011-08-17 21:45:34 -0400

    initial import of Task::BeLike::DAGOLDEN

---
+++ lib/Task/BeLike/DAGOLDEN.pm
@@ -0,0 +1,26 @@
+use 5.006;
+use strict;
+use warnings;
+
+package Task::BeLike::DAGOLDEN;
+# VERSION
+
+1;
+
+# ABSTRACT: No abstract given for Task::BeLike::DAGOLDEN
+
+=pod
+
+=pkgroup Modules That are Useful
+
+=pkg Package::Name  0  need at least version 0
+
+=head1 USAGE
+
+This module has no direct functionality.  It is merely a placeholder
+for prerequisites.  Installing it will ensure all the prerequisites listed
+above are satisfied.
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
