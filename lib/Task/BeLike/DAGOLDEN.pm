3ad40a390af27b81af475064133835f7dec688ff
David Golden <dagolden@cpan.org>
2011-08-17 22:25:36 -0400

    start recording modules I use

--- lib/Task/BeLike/DAGOLDEN.pm
+++ lib/Task/BeLike/DAGOLDEN.pm
@@ -7,13 +7,71 @@ package Task::BeLike::DAGOLDEN;
 
 1;
 
-# ABSTRACT: No abstract given for Task::BeLike::DAGOLDEN
+# ABSTRACT: DAGOLDEN's frequently-used or favorite modules
 
 =pod
 
-=pkgroup Modules That are Useful
+=pkgroup Apps
 
-=pkg Package::Name  0  need at least version 0
+=pkg App::Ack
+
+=pkg App::cpanminus
+
+=pkg App::grindperl
+
+
+=pkgroup CLI tools
+
+=pkg Getopt::Lucid
+
+
+=pkgroup Filesystem tools
+
+=pkg Path::Class
+
+=pkg File::Find::Rule
+
+=pkg File::Find::Rule::Perl
+
+
+=pkgroup Testing Modules
+
+=pkg Test::More  0.96   Minimally sane subtest()'s
+
+=pkg Test::Deep
+
+=pkg Test::Differences
+
+=pkg Test::Fatal 0 Better than Test::Exception
+
+=pkg Test::Routine 0 Better than Test::Class
+
+
+=pkgroup Perl development tools
+
+=pkg Dist::Zilla 4
+
+=pkg Dist::Zilla::PluginBundle::DAGOLDEN 0.018
+
+=pkg Devel::NYTProf
+
+=pkg CPAN::Uploader
+
+
+=pkgroup Email tools
+
+=pkg Email::Sender
+
+
+=pkgroup Web tools
+
+=pkg Bundle::LWP
+
+=pkg HTTP::Tiny
+
+=pkg WWW::Mechanize
+
+
 
 =head1 USAGE
 
